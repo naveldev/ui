2f4eb9ad793be190f05321841e3bf6fa56488aac
Naviisml <43450192+naviisml@users.noreply.github.com>
2021-04-16 22:31:01 +0200

    Removed unnecessary code

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -9,4 +9,3 @@
 export { default as Slider } from "./Slider/Slider.vue";
 export { default as SliderItem } from "./Slider/Item.vue";
 export { default as Dropdown } from "./Dropdown.vue";
-export { default as FormError } from "./Form/Error.vue";
