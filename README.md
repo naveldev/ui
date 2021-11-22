e808dbb3fa351c0b242d77d2720f65f51ec5afd9
Navi <mailvoornavi@gmail.com>
2021-11-22 04:39:39 +0100

    fixes

--- README.md
+++ README.md
@@ -2,6 +2,21 @@
 
 Comming soon...
 
-## Installation
+## NPM
 
-...
+Step 1: Install the package.
+
+```
+npm install https://github.com/naveldev/ui.git
+```
+
+Step 2: Include the complete CSS library using ES6
+
+```
+import "@naveldev/ui";
+```
+
+Or include specific modules you would like to include;
+```
+import { Component, Component2 } from "@naveldev/ui";
+```
--- src/js/index.js
+++ src/js/index.js
@@ -6,22 +6,5 @@
  * Copyrights (c) navel.dev 2020
  */
 
-import * as Components from "./components";
-import Forms from "./plugins/forms";
-
-export class Class
-{
-	static install(Vue, options = {})
-	{
-		this.registerComponents(Vue)
-	}
-
-	static registerComponents(Vue)
-	{
-		Object.values(Components).forEach(c => Vue.component(c.name, c))
-	}
-}
-
-export const Form = Forms
-
-export const NavelUI = Class
+console.log("test");
+export { default as App } from '../scss/app.scss'
